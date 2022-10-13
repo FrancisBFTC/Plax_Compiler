c98b1a9585102f844e69c5d8e96f6f4d4a890c8e
FrancisBFTC <bftcorporations@gmail.com>
2022-10-13 12:52:50 -0300

    Update: Instalador - Configura Syntax e Registros do Windows

--- Config/Registry.bat
+++ Config/Registry.bat
@@ -5,9 +5,9 @@ cls
 REG ADD HKCR\plax.file /t REG_SZ /d "PLAX Programming File"
 REG ADD HKCR\plax.file\DefaultIcon /t REG_SZ /d "C:\\Plax_Compiler\\icon.png"
 REG ADD HKCR\plax.file\shell\Edit /t REG_SZ /d "&Editar"
-REG ADD HKCR\plax.file\shell\Edit\command /t REG_SZ /d "C:\\Program Files\\Notepad++\\notepad++.exe %1"
+REG ADD HKCR\plax.file\shell\Edit\command /t REG_SZ /d "C:\\Program Files\\Notepad++\\notepad++.exe %%1"
 REG ADD HKCR\plax.file\shell\Open /t REG_SZ /d "&Abrir"
-REG ADD HKCR\plax.file\shell\Open\command /t REG_SZ /d "C:\\Plax_Compiler\\PlaxLang\\plax.exe %1"
+REG ADD HKCR\plax.file\shell\Open\command /t REG_SZ /d "C:\\Plax_Compiler\\PlaxLang\\plax.exe %%1"
 
 :: Cria e Configura uma chave de extensão no ROOT
 REG ADD HKCR\.plax /t REG_SZ /d "plax.file"
@@ -20,9 +20,9 @@ REG ADD HKCR\.plax\OpenWithProgids /v plax.file /t REG_BINARY
 
 :: Cria e Configura uma chave de extensão no USER
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /t REG_SZ /d "plax.exe"
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "a" /t REG_SZ /d "Notepad++.exe"
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "b" /t REG_SZ /d "SublimeText.exe"
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "c" /t REG_SZ /d "VSCode.exe"
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "a" /t REG_SZ /d "C:/Program Files/Notepad++/Notepad++.exe"
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "b" /t REG_SZ /d "C:/Program Files/Sublime Text/sublime_text.exe"
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "c" /t REG_SZ /d "code.exe"
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithList /v "MRUList" /t REG_SZ /d "abc"
 
 REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.plax\OpenWithProgids /t REG_SZ /d "plax.file"
